13249b2b4e1eb3991bc71225a73408068fa73042
youssef mostafa rabea <Youssefrabea3@gmail.com>
2025-12-13 02:52:57 +0200

    Revert "Fix rally point and placement cancellation issues"
    This reverts commit 531187300aaebaa06e138b7c6bfdfc63afd0db5e.

--- Assets/Scripts/RTSBuildingsSystems/BuildingSelectionManager.cs
+++ Assets/Scripts/RTSBuildingsSystems/BuildingSelectionManager.cs
@@ -30,10 +30,6 @@ namespace RTS.Buildings
         [SerializeField] private bool enableDoubleClick = true;
         [SerializeField] private float doubleClickTime = 0.3f;
 
-        [Header("Audio")]
-        [SerializeField] private AudioClip rallyPointSetSFX;
-        [SerializeField] private float rallyPointSFXVolume = 1f;
-
         [Header("Debug")]
         [SerializeField] private bool enableDebugLogs = true;
 
@@ -386,20 +382,6 @@ namespace RTS.Buildings
 
         private void OnRightClick(InputAction.CallbackContext context)
         {
-            // Don't process right-clicks if currently placing a building or wall
-            // (right-click should cancel placement instead)
-            if (buildingManager != null && buildingManager.IsPlacingBuilding)
-            {
-                if (enableDebugLogs)
-                return;
-            }
-
-            if (wallPlacementController != null && wallPlacementController.IsPlacingWalls)
-            {
-                if (enableDebugLogs)
-                return;
-            }
-
             // Only process right-clicks if a building is selected
             if (CurrentlySelectedBuilding == null)
             {
@@ -462,12 +444,7 @@ namespace RTS.Buildings
                         rallyFlag.SetRallyPointPosition(hit.point);
                         rallyFlag.ShowFlag(); // Show flag when rally point is set
                     }
-
-                    // Play rally point set SFX
-                    if (rallyPointSetSFX != null)
-                    {
-                        AudioSource.PlayClipAtPoint(rallyPointSetSFX, hit.point, rallyPointSFXVolume);
-                    }
+                    
 
                 }
                
